18eae11137ffc1f2ae6f761377435e516b18b61f
cascci123 <71568834+cascci123@users.noreply.github.com>
2021-02-20 15:50:03 -0800

    Add files via upload

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import sys  # In order to terminate the program
 
 def webserver(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(('', port))
+    serverSocket.bind(('localhost', port))
     serverSocket.listen(5)
     while True:
         connectionSocket, addr = serverSocket.accept()
@@ -15,7 +15,7 @@ def webserver(port=13331):
             f = open(filename[1:])
             outputdata = f.read()
             f.close()
-            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+            connectionSocket.send("HTTP/1.0 200 OK\r\n".encode())
             connectionSocket.send("Content-Type: text/html\r\n\r\n".encode())
 
             #for i in range(0, len(outputdata)):
@@ -24,7 +24,7 @@ def webserver(port=13331):
             connectionSocket.close()
 
         except IOError:
-            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+            connectionSocket.send("HTTP/1.0 404 Not Found\r\n\r\n".encode())
             connectionSocket.send("File Not Found".encode())
             connectionSocket.close()
 
