60d25df222a14fb4589a7799272b3bbf80ea3ff8
cascci123 <71568834+cascci123@users.noreply.github.com>
2021-02-20 17:25:47 -0800

    Add files via upload

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import sys  # In order to terminate the program
 
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(("", port))
+    serverSocket.bind((gethostname("localhost"), port))
     serverSocket.listen(5)
     while True:
         connectionSocket, addr = serverSocket.accept()
@@ -14,8 +14,7 @@ def webServer(port=13331):
             filename = message.split()[1]
             f = open(filename[1:])
             outputdata = f.read()
-            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
-            connectionSocket.send("Content-Type: text/html\r\n\r\n".encode())
+            connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".encode())
 
             #for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata.encode())
